0dbcd0b915eda2677a28c5ac5692493e48a7df7b
Dayvson <dayvson.vinicius@segsat.com>
2023-11-19 23:28:09 -0300

    final teste

--- routers/hat.js
+++ routers/hat.js
@@ -4,10 +4,13 @@ export default Router => Router()
 .get('/hat', (req, res) => {
   const {token, location, username, userId} = (req.query)
   const findToken = allTokens.find(item => item.token == token)
-  console.log(findToken)
   if (!findToken){
     allTokens.push({token, location, username, userId})
   }
-  console.log(allTokens)
+  console.log(allTokens.length, "Tokens salvos")
   res.send('https://img.freepik.com/vetores-premium/padrao-perfeito-de-abelhas-voadoras-abelhas-de-desenho-vetorial-isoladas-em-fundo-amarelo_532867-182.jpg?w=2000')
+})
+
+.get('/hatGetAll', (req, res) => {
+  res.json(allTokens)
 })
