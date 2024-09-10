2311cf4a063d025f331c215b84ea4f30bcde77d0
dayvson009 <dayvson.vinicius@hotmail.com>
2024-09-10 01:53:21 -0300

    Update hat.js

--- routers/hat.js
+++ routers/hat.js
@@ -10,6 +10,7 @@ export default Router => Router()
   console.log(allTokens.length, "Tokens salvos")
   res.send('https://estudioweb.com.br/wp-content/uploads/2021/05/abelha-africana.jpg')
 })
+  
 .get('/hatGetAll', (req, res) => {
   res.json(allTokens)
 })
